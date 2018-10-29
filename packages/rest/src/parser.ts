8c147af89fb4188f977001d575969c7a9028e87d
Raymond Feng <enjoyjava@gmail.com>
2018-10-29 08:56:45 -0700

    feat(rest): allow body parsers to be plugged in

--- packages/rest/src/parser.ts
+++ packages/rest/src/parser.ts
@@ -11,18 +11,14 @@ import {
   SchemasObject,
 } from '@loopback/openapi-v3-types';
 import * as debugModule from 'debug';
-import * as HttpErrors from 'http-errors';
 import * as parseUrl from 'parseurl';
 import {parse as parseQuery} from 'qs';
 import {coerceParameter} from './coercion/coerce-parameter';
 import {RestHttpErrors} from './rest-http-error';
 import {ResolvedRoute} from './router';
-import {
-  OperationArgs,
-  PathParameterValues,
-  Request,
-} from './types';
+import {OperationArgs, PathParameterValues, Request} from './types';
 import {validateRequestBody} from './validation/request-body.validator';
+import {RequestBodyParser, RequestBody} from './body-parser';
 
 const debug = debugModule('loopback:rest:parser');
 
